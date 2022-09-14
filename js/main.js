734dbf6c5a626e99ca6b955025aa143868ef6b7e
Rebhe <rebheibrahim@gmail.com>
2022-09-15 01:06:36 +0300

    Upload The design

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let toggle = document.querySelector(".toggle");
+let topbar = document.querySelector(".topbar");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+let themeSwitch = document.querySelector(".themeSwitch");
+let body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  toggle.classList.toggle("active");
+  topbar.classList.toggle("active");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+});
+
+// theme switch toggle
+
+themeSwitch.addEventListener("click", function () {
+  body.classList.toggle("dark");
+});
+
+// now, we fix menu active class issue for mobile devices
+
+function toggleMenu() {
+  let navigation = document.querySelector(".navigation");
+  let main = document.querySelector(".main");
+  navigation.classList.remove("active");
+  main.classList.remove("active");
+}
+
+document.querySelector(".link").addEventListener("click", toggleMenu);
+
+// current year in copyright div
+
+document.querySelector(".year").innerHTML = new Date().getFullYear();
